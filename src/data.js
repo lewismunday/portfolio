7ed8df23f7c3d96940ebea13ca52640e82825b48
Lewis Munday <lewisjohnmunday@gmail.com>
2022-08-08 23:30:00 +0100

    Added

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const projects = [
+    {
+        title: "CSV to JSON",
+        subtitle: "Convert CSV to JSON",
+        description:
+            "A lightweight, online CSV to Dictionary conversion tool",
+        image: "./csv2d.png",
+        link: "https://www.lewismunday.co.uk/csv2d",
+        source: "https://github.com/lewismunday/CSV2D",
+    }
+];
